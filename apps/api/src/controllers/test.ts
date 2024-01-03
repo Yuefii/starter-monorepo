96835f7629fc7917586fb082d6ccef46b36d33fb
yuefii <muffidahmadiawan31@gmail.com>
2024-01-03 20:38:54 +0700

    add eslint on apps/api, prettier && test format prettier

--- apps/api/src/controllers/test.ts
+++ apps/api/src/controllers/test.ts
@@ -1,9 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 export const getWelcomeMessage = (req: Request, res: Response) => {
   const data = {
     title: 'WELCOME TO SETUP MONOREPO',
     subtitle: 'COMING SOON'
-  };
-  res.send(data);
-};
+  }
+  res.send(data)
+}
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,37 +1,34 @@
-import { Button } from "@/components/ui/button";
-import Head from "next/head";
-import { useEffect, useState } from "react";
+import { Button } from '@/components/ui/button'
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
 
 interface DataProps {
-  title: string;
-  subtitle: string;
+  title: string
+  subtitle: string
 }
 
 const Home = () => {
-  const [data, setData] = useState<DataProps>();
+  const [data, setData] = useState<DataProps>()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("http://localhost:8080");
-        const fetchedData = await res.json();
-        setData(fetchedData);
+        const res = await fetch('http://localhost:8080')
+        const fetchedData = await res.json()
+        setData(fetchedData)
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <>
       <Head>
         <title>Starter Monorepo</title>
-        <meta
-          name="Starter Monorepo"
-          content="Ini adalah halaman utama setup monorepo"
-        />
+        <meta name="Starter Monorepo" content="Ini adalah halaman utama setup monorepo" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className="min-h-screen flex justify-center items-center">
@@ -39,22 +36,20 @@ const Home = () => {
           {data ? (
             <>
               <h1 className="text-4xl text-primary font-bold">{data.title}</h1>
-              <p className="text-lg text-center text-gray-500 py-2">
-                {data.subtitle}
-              </p>
+              <p className="text-lg text-center text-gray-500 py-2">{data.subtitle}</p>
             </>
           ) : (
             <p className="text-center text-2xl text-primary">Loading</p>
           )}
           <div className="flex justify-center items-center">
-            <Button onClick={() => alert("Oke get started")} size="lg">
+            <Button onClick={() => alert('Oke get started')} size="lg">
               Get Started
             </Button>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
